9432f097d3c9ae031ea2e12b6eab69172112399a
Roman <litl_rom@tut.by>
2019-12-08 16:04:10 +0300

    address completed

--- Task 5/Address/Address/Address.cs
+++ Task 5/Address/Address/Address.cs
@@ -5,57 +5,35 @@ using System.Text;
 namespace Address
 {
     class Address
-    {
-        private int index, house, apartment;
-        private string country, city, street;
+    {   
+        public int Index { get; set; }
+        
+        public int House { get; set; }
 
-        public int Index
-        {
-            get { return index; }
-            set { index = value; }
-        }
-        public int House
-        {
-            get { return house; }
-            set { house = value; }
-        }
-        public int Apartment
-        {
-            get { return apartment; }
-            set { apartment = value; }
-        }
+        public int Apartment { get; set; }
 
-        public string Country
-        {
-            get { return country; }
-            set { country = value; }
-        }
-        public string City
-        {
-            get { return city; }
-            set { city = value; }
-        }
-        public string Street
-        {
-            get { return street; }
-            set { street = value; }
-        }
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
 
         public Address (string country, string city, string street, 
                         int house, int index, int apartment)
         {
-            this.country = country;
-            this.city = city;
-            this.street = street;
-            this.house = house;
-            this.index = index;
-            this.apartment = apartment;
+            Country = country;
+            City = city;
+            Street = street;
+            House = house;
+            Index = index;
+            Apartment = apartment;
         }
 
         internal void Show ()
         {
             Console.WriteLine(" Страна - {0} \n Город - {1} \n Улица - {2} \n Дом № - {3} \n " +
-                "Индекс - {4} \n Квартира № - {5}", country, city, street, house, index, apartment);
+                "Индекс - {4} \n Квартира № - {5}", Country, City, Street, House, Index, Apartment);
         }
     }
 }
